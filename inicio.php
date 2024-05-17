b6cfe194e226e21f4b06370365ced0962922efc8
Carvalx <carvadel@hotmail.com>
2024-05-17 10:42:07 +0200

    segundo commit version2

--- inicio.php
+++ inicio.php
@@ -5,15 +5,80 @@
             <p>"Brindando lealtad y confianza inquebrantables a la salud y bienestar de tu fiel compañero."</p>
         </div>
     </div>
-    <div class="divisor-seccion">
-        <p>~~~~~</p><img src="img/gatito-icono.png" title="Gatito" alt="No image" class="icono-seccion"><p>~~~~~</p>
-    </div>
+    
     <section>
         <h1>Clínica Veterinaria Clarity Pet ubicada en Valencia</h1>
         <p>Explora un enfoque renovado de atención y vínculo afectivo en nuestra Clínica Veterinaria en Valencia.</p>
     </section>
 
-    <section class="servicios-inicio">
+    <div class="divisor-seccion">
+        <p>~~~~~~~</p><img src="img/gatito-icono.png" title="Gatito" alt="No image" class="icono-seccion"><p>~~~~~~~</p>
+    </div>
+
+    <section>
         <h2>Nuestros Servicios</h2>
+
+        <div class="servicios-inicio">
+            <!-- Primera fila -->
+            <div>
+                <a href="">
+                    <img src="img/medicina-general.png" alt="No image" title="Medicina General">
+                    <h3>Medicina general</h3>
+                </a>
+                <p>Ofrecemos una amplia gama de servicios, que incluyen vacunación, colocación de microchips, cuidado dental, desparasitación, entre otros.</p>
+                
+            </div>
+            <div>
+                <a href="">
+                    <img src="img/diagnostico.png" alt="No image" title="Diagnóstico">
+                    <h3>Diagnóstico profunda</h3>
+                </a>
+                <p>Disponemos de un avanzado equipo de radiografía de última generación con tecnología de revelado digital.</p>
+            </div>
+            <div>
+                <a href="">
+                    <img src="img/cirugias.png" alt="No image" title="Cirugías">
+                    <h3>Cirugías</h3>
+                </a>
+                <p>Contamos con un quirófano completamente equipado, que incluye una unidad anestésica y equipos de monitorización.</p>
+            </div>
+            <div>
+                <a href="">
+                    <img src="img/microscopio.png" alt="No image" title="Laboratorio">
+                    <h3>Laboratorio</h3>
+                </a>
+                <p>Ofrecemos una amplia gama de servicios de laboratorio veterinario, que incluyen análisis de hemogramas, bioquímica, detección de parásitos sanguíneos. </p>
+            </div>
+
+            <!-- Segunda fila -->
+            <div>
+                <a href="">
+                    <img src="img/revision.png" alt="No image" title="Asesoramiento">
+                    <h3>Asesoramiento</h3>
+                </a>
+                <p>nuestros servicios completos que incluyen vacunación, emisión de pasaportes para viajes, y recomendaciones personalizadas para adopciones responsables.</p>
+            </div>
+            <div>
+                <a href="">
+                    <img src="img/comida.png" alt="No image" title="Alimentación">
+                    <h3>Alimentación</h3>
+                </a>
+                <p>Descubre una alimentación adaptada a cada necesidad y una amplia gama de accesorios y complementos para el bienestar de tu mascota.</p>
+            </div>
+            <div>
+                <a href="">
+                    <img src="img/laserterapia.png" alt="No image" title="Laserterapia">
+                    <h3>Laserterapia</h3>
+                </a>
+                <p>tratamientos indoloros diseñados para promover el bienestar y la salud de todas las mascotas.</p>
+            </div>
+            <div>
+                <a href="">
+                    <img src="img/adiestramiento.png" alt="No image" title="Adiestramiento">
+                    <h3>Adiestramiento</h3>
+                </a>
+                <p>Valoramos la importancia de esta disciplina para ofrecer un cuidado integral y personalizado a cada uno de nuestros pacientes.</p>
+            </div>
+        </div>
     </section>
 </div>
